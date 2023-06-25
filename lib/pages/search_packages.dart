a2d080393cda5a50be92c28640ef7df96c3a4aa2
psaul20 <patricksaul20@gmail.com>
2023-06-25 18:44:48 -0500

    get collections with riverpod

--- lib/pages/search_packages.dart
+++ lib/pages/search_packages.dart
@@ -3,48 +3,48 @@ import 'dart:core';
 
 import 'package:democratus/api/govinfo_api.dart';
 import 'package:democratus/models/collection.dart';
-import 'package:democratus/models/search_state.dart';
-import 'package:democratus/redux/actions.dart';
-import 'package:democratus/redux/reducers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// Basing redux implementation off of https://www.youtube.com/watch?v=Wj216eSBBWs
-// TODO: Implement middleware for async data fetching
-class SearchPackages extends StatelessWidget {
+//Basing Riverpod implementation off of https://www.youtube.com/watch?v=Zp7
+final collectionsProvider = FutureProvider<List<Collection>>((ref) async {
+  CollectionList collections = await GovinfoApi().getCollections();
+  return collections.asList;
+});
+
+final selectedCollectionProvider = StateProvider<Collection?>((ref) {
+  final collections = ref.watch(collectionsProvider);
+  collections.whenData((data) => data.first);
+  return null;
+});
+
+class SearchPackages extends ConsumerWidget {
   const SearchPackages({super.key});
 
   // submitSearch() async {
   @override
-  Widget build(BuildContext context) {
-    final Store<SearchState> store = Store<SearchState>(searchStateReducer,
-        initialState: SearchState.initialState());
-    return StoreProvider<SearchState>(
-      store: store,
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ProviderScope(
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Search Documents"),
         ),
-        body: StoreConnector<SearchState, ViewModel>(
-          converter: (Store<SearchState> store) => ViewModel.create(store),
-          builder: (BuildContext context, ViewModel vm) => Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Choose a Collection:",
-                  textAlign: TextAlign.left,
-                ),
-                CollectionsDropDownBuilder(vm: vm),
-                const Text('Published after date:'),
-                InputDatePickerFormField(
-                  firstDate: DateTime(1900, 1, 1),
-                  lastDate: DateTime.now(),
-                ),
-              ],
-            ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Choose a Collection:",
+                textAlign: TextAlign.left,
+              ),
+              const CollectionsDropDownBuilder(),
+              const Text('Published after date:'),
+              InputDatePickerFormField(
+                firstDate: DateTime(1900, 1, 1),
+                lastDate: DateTime.now(),
+              ),
+            ],
           ),
         ),
         // floatingActionButton: const SearchButtonBuilder(),
@@ -55,64 +55,19 @@ class SearchPackages extends StatelessWidget {
   }
 }
 
-class ViewModel {
-  final List<Collection> collections;
-  // final Map<String, String> queryParams;
-  final Collection? selectedCollection;
-  final Function(List<Collection>) onGetCollections;
-  final Function(Collection) onSelectCollection;
-
-  ViewModel({
-    this.selectedCollection,
-    required this.collections,
-    required this.onGetCollections,
-    required this.onSelectCollection,
-  });
-
-  factory ViewModel.create(Store<SearchState> store) {
-    onGetCollections(List<Collection> collections) {
-      store.dispatch(GetCollectionsAction(collections));
-      store.dispatch(SelectCollectionAction(collections.first));
-    }
-
-    onSelectCollection(Collection collection) {
-      store.dispatch(SelectCollectionAction(collection));
-    }
-
-    return ViewModel(
-      collections: store.state.collections,
-      onGetCollections: onGetCollections,
-      onSelectCollection: onSelectCollection,
-    );
-  }
-}
-
-class CollectionsDropDownBuilder extends StatefulWidget {
-  const CollectionsDropDownBuilder({super.key, required this.vm});
-  final ViewModel vm;
+class CollectionsDropDownBuilder extends ConsumerWidget {
+  const CollectionsDropDownBuilder({super.key});
 
   @override
-  CollectionsDropDownBuilderState createState() =>
-      CollectionsDropDownBuilderState();
-}
-
-class CollectionsDropDownBuilderState
-    extends State<CollectionsDropDownBuilder> {
-  @override
-  void initState() {
-    CollectionList collections = GovinfoApi().getCollections();
-    widget.vm.onGetCollections(collections.asList);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<Collection>> collections = ref.watch(collectionsProvider);
+    String? dropDownValue =
+        ref.watch(selectedCollectionProvider)?.collectionName;
     //TODO: Error handling
-    Widget returnWidget;
-    if (widget.vm.collections.isNotEmpty) {
-      returnWidget = DropdownButton<String>(
+    return collections.when(data: ((collections) {
+      return DropdownButton<String>(
         isExpanded: true,
-        value: widget.vm.selectedCollection?.collectionName,
+        value: dropDownValue,
         icon: const Icon(Icons.arrow_downward),
         elevation: 16,
         style: const TextStyle(color: Colors.deepPurple),
@@ -121,21 +76,23 @@ class CollectionsDropDownBuilderState
           color: Colors.deepPurpleAccent,
         ),
         onChanged: (String? value) {
-          int idx = widget.vm.collections
+          int idx = collections
               .indexWhere((element) => element.collectionName == value);
-          widget.vm.onSelectCollection(widget.vm.collections[idx]);
+          ref.read(selectedCollectionProvider.notifier).state =
+              collections.elementAt(idx);
         },
-        items: widget.vm.collections.map<DropdownMenuItem<String>>((element) {
+        items: collections.map<DropdownMenuItem<String>>((element) {
           return DropdownMenuItem<String>(
             value: element.collectionName,
             child: Text(element.collectionName),
           );
         }).toList(),
       );
-    } else {
-      returnWidget = const Text("Fetching Data...");
-    }
-    return returnWidget;
+    }), error: (Object error, StackTrace stackTrace) {
+      return (Text("{$error}"));
+    }, loading: () {
+      return const Text("Fetching Collections");
+    });
   }
 }
 
