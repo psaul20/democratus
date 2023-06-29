b43b16042d43554ad8bc250baabf4be33c60955f
psaul20 <patricksaul20@gmail.com>
2023-06-28 23:33:34 -0500

    added read more button

---
+++ lib/pages/package_reader.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PackageReader extends ConsumerWidget {
+  const PackageReader({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Card(),
+    );
+  }
+}
--- lib/widgets/package_widgets.dart
+++ lib/widgets/package_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:democratus/main.dart';
+import 'package:democratus/pages/package_reader.dart';
 import 'package:democratus/styles/text_styles.dart';
 import 'package:democratus/widgets/buttons.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,7 @@ class PackageTile extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(savedPackagesProvider);
-    onTap() {
+    saveTap() {
       if (package.isSaved) {
         ref.read(savedPackagesProvider.notifier).removePackage(package);
         package.isSaved = false;
@@ -42,33 +43,114 @@ class PackageTile extends ConsumerWidget {
       }
     }
 
-    return Card(
-      child: ExpansionTile(
-        title: Text(package.title),
-        tilePadding: const EdgeInsets.only(left: 10, bottom: 0, right: 10),
-        children: [
-          ListTile(
-            contentPadding: const EdgeInsets.only(left: 10),
-            dense: true,
-            title: Column(
-              children: [
-                Row(
-                  children: [
-                    Text(
+    readMore() {
+      Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) {
+              return const PackageReader();
+            },
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return SlideTransition(
+                position: Tween<Offset>(
+                  begin: const Offset(1.0, 0.0),
+                  end: Offset.zero,
+                ).animate(animation),
+                child: child,
+              );
+            },
+          ));
+    }
+
+    return Hero(
+      tag: 'cardToFullScreen${package.packageId}',
+      child: Card(
+        child: ExpansionTile(
+          title: Text(package.title),
+          tilePadding: const EdgeInsets.only(left: 10, bottom: 0, right: 10),
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.only(
+                left: 10,
+              ),
+              dense: true,
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Author: ${package.governmentAuthor1 ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Category: ${package.category ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Type: ${package.billType ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Branch: ${package.branch ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Origin Chamber: ${package.originChamber ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Current Chamber: ${package.currentChamber ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Pages: ${package.pages?.toString() ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Publisher: ${package.publisher ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Congress: ${package.congress ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Text(
+                    "Bill Number: ${package.billNumber?.toString() ?? "Unknown"}",
+                    textAlign: TextAlign.start,
+                    style: TextStyles.expandedListTile,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: Text(
                       "Last Action: ${DateFormat.yMMMd().format(package.lastModified)}",
                       textAlign: TextAlign.start,
                       style: TextStyles.expandedListTile,
                     ),
-                  ],
-                ),
-                SaveButton(
-                  isSaved: package.isSaved,
-                  onTap: onTap,
-                )
-              ],
-            ),
-          )
-        ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SaveButton(
+                        isSaved: package.isSaved,
+                        onTap: saveTap,
+                      ),
+                      ReadMoreButton(onTap: readMore)
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
