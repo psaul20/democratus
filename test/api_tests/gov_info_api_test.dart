2a43c106776e41e0dade1333a7cfb78ea745f014
psaul20 <patricksaul20@gmail.com>
2023-09-03 23:44:23 -0500

    added keyword search with offset test

--- test/api_tests/gov_info_api_test.dart
+++ test/api_tests/gov_info_api_test.dart
@@ -36,5 +36,37 @@ void main() async {
       expect(List.from(json1['results'])[0].keys,
           List.from(json2['results'])[0].keys);
     });
+    //Test offset
+    test('Testing search bills by keyword with offset', () async {
+      Response response1 =
+          await govinfoApi.searchBillsByKeyword(keyword: 'megahertz');
+      expect(response1.statusCode, 200);
+      Map<String, dynamic> jsonResponse1 = jsonDecode(response1.body);
+      Response response2 = await govinfoApi.searchBillsByKeyword(
+          keyword: 'megahertz', resetOffset: false);
+
+      expect(response2.statusCode, 200);
+      Map<String, dynamic> jsonResponse2 = jsonDecode(response2.body);
+
+            expect(jsonResponse1.keys, jsonResponse2.keys);
+
+      List<String> billIds1 = List<String>.from(jsonResponse1['results']
+          .map((bill) => bill['packageId'].toString())
+          .toList());
+
+      List<String> billIds2 = List<String>.from(jsonResponse2['results']
+          .map((bill) => bill['packageId'].toString())
+          .toList());
+
+      for (String billId in billIds2) {
+        expect(billIds1.contains(billId), false);
+      }
+
+      Map<String, dynamic> jsonExample = jsonDecode(
+          File('${Strings.billFilePath}/govinfo_bill_search_example.json')
+              .readAsStringSync());
+
+      expect(jsonExample.keys, jsonResponse2.keys);
+    });
   });
 }
